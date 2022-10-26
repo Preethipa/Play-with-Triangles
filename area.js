d1b1e99ac568241fb098f0f37bbb6fb668f74b65
PREETHI <preethirajavel06@gmail.com>
2022-10-26 16:54:12 +0530

    area of triangle is done

---
+++ area.js
@@ -0,0 +1,21 @@
+const areas = document.querySelectorAll(".area-input");
+const areaBtn = document.querySelector("#area-btn");
+const outputArea = document.querySelector("#output-area");
+
+function calculateAreaOfTriangle(base, height){
+    const areaOfTriangle = (base * height);
+    //console.log(areaOfTriangle);
+    return areaOfTriangle;
+}
+
+function calculateArea(){
+    const areaOfTriangle = calculateAreaOfTriangle(Number(areas[0].value),Number(areas[1].value))
+    //calculateAreaOfTriangle(2,3);
+    //console.log(areaOfTriangle);
+    const areaOfTheTriangle = 1/2*( areaOfTriangle);
+    outputArea.innerText = "Area of the triangle is " + areaOfTheTriangle+"cm²";
+}
+
+
+
+areaBtn.addEventListener('click', calculateArea)
