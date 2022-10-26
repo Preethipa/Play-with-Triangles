46796d0580c87a2fd77e6463e4f60aef33619bb6
PREETHI <preethirajavel06@gmail.com>
2022-10-26 12:43:47 +0530

    correcting error

--- quiz.js
+++ quiz.js
@@ -1,21 +1,21 @@
-const quizForm = document.querySelectorAll(".quiz-form");
+const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
-const outpuText = document.querySelector("#output-text");
+const outputText = document.querySelector("#output-text");
 
 const correctAnswers = ["90°"," right angled"];
 
 function calculateScore(){
     let score = 0;
     let index = 0;
-    var data = new FormData(quizForm);
-    for(let value of data.values()){
-       // console.log(value);
-       if(value === correctAnswers){
+    const fromResults = new FormData(quizForm);
+    for(let value of formResuls.values()){
+    //console.log(value);
+        if(value === correctAnswers[index]){
             score = score + 1;
-       }
-       index =  index + 1;
+         }
+        index = index + 1;
     }
-    console.log("ur score is "+ score);
+    console.log( score);
+     //outputText.innerText = "Your score is " + score;
 }
-
 submitAnswerBtn.addEventListener('click', calculateScore);
